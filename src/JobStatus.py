f238aed6a72683fe3fc0870ff0545a70bb81c49c
shawn <shawn24601@gmail.com>
2016-08-17 19:36:09 +0800

    init some class

---
+++ src/JobStatus.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-15 -*-
+
+class JobStatus:
+    def __init__():
+        # init yarn api client
+        return
+
+    def getStepStatus():
+        # get latest step status
+        # TODO: how to store step status
+        return
---
+++ src/StepExecutor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-15 -*-
+
+class StepExecutor:
+    def execDistribution():
+        return
+
+    def execAlign():
+        return
+
+    def execVariation():
+        return
+
+    def execPkgResult():
+        return
+
+    def execQc():
+        return
