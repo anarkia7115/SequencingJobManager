65ffa8c2531d75d4c8a7ec5ce8f4d1cbe74d22a9
shawn <shawn24601@gmail.com>
2016-08-22 17:30:19 +0800

    delete not used class

--- src/executor.py
+++ src/executor.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python
 
-class CommonExecutor():
-    def __init__(self, execType, args):
-        if execType == 'cl':
-            return CommandLineExecutor(args)
-        elif execType == 'ha':
-            return HadoopAppExecutor(args)
-
 class CommandLineExecutor():
     def __init__(self, args):
         self.args = args
